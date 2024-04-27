fbd095a827de19a2adadef2a6a19202254577e0f
moetez96 <moetez.ayari@esprit.tn>
2024-04-27 22:47:14 +0100

    Fix audio player slider speed

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -13,9 +13,10 @@ function MusicPlayer() {
   const [duration, setDuration] = useState(0);
   const [fileList, setFileList] = useState([]);
   const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
 
   useEffect(() => {
-    document.documentElement.style.setProperty("--slider-value", value);
+    document.documentElement.style.setProperty("--slider-value-volume", value);
     audioRef.current.volume = value / 100;
   }, [value]);
 
@@ -28,8 +29,6 @@ function MusicPlayer() {
     setValue(event.target.value);
   };
 
-  const audioRef = useRef(null);
-
   const playAudio = () => {
     if (isPlaying) {
       audioRef.current.pause();
@@ -40,27 +39,39 @@ function MusicPlayer() {
   };
 
   const handleSliderChange = (event) => {
-    audioRef.current.currentTime =
-      (event.target.value * audioRef.current.duration) / 100;
+    audioRef.current.currentTime = (event.target.value / 100) * duration;
+    setCurrentTime(event.target.value);
+    document.documentElement.style.setProperty("--slider-value-duration", currentTime);
   };
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
-    const url = URL.createObjectURL(file);
-    audioRef.current.src = url;
-    setFileList((prevFileList) => [...prevFileList, url]);
-    localStorage.setItem("fileList", JSON.stringify([...fileList, url]));
+    if (file.type.startsWith("audio/")) {
+      const url = URL.createObjectURL(file);
+      audioRef.current.src = url;
+      setFileList((prevFileList) => [...prevFileList, url]);
+      localStorage.setItem("fileList", JSON.stringify([...fileList, url]));
+      
+      audioRef.current.addEventListener('loadedmetadata', () => {
+        setDuration(audioRef.current.duration);
+      });
+    } else {
+
+      // Handle error
+      console.error("Invalid file type. Please select an MP3 file.");
+    }
   };
 
   const updateTime = () => {
-    setCurrentTime(audioRef.current.currentTime);
-    setDuration(audioRef.current.duration);
+    setCurrentTime((audioRef.current.currentTime / duration) * 100);
+    document.documentElement.style.setProperty("--slider-value-duration", currentTime);
   };
 
   return (
     <div className="player-wrapper">
       <div>
-        <input type="file" accept="audio/mp3" onChange={handleFileChange} />
+        <input type="file" accept="audio/mp3" onChange={handleFileChange}           style={{ display: "none" }}
+/>
         <audio
           ref={audioRef}
           controls
@@ -87,10 +98,10 @@ function MusicPlayer() {
             <SkipFwdIcon />
           </div>
           <div className="music-playing-slider">
-            <input
+          <input
               type="range"
               min={0}
-              max={duration}
+              max={100}
               value={currentTime}
               onChange={handleSliderChange}
               className="music-playing-sliders"

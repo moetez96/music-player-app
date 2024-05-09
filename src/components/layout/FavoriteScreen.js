a88492c0aba743a84fe4f0ada4ffe1332184f36c
moetez96 <moetez.ayari@esprit.tn>
2024-05-10 00:37:05 +0100

    Add favotireScreen component

--- src/components/layout/FavoriteScreen.js
+++ src/components/layout/FavoriteScreen.js
@@ -1,10 +1,42 @@
 // MainScreen.js
 import React, { useEffect, useState } from "react";
+import UploadMusic from "../buttons/UploadMusic";
+import ScanForMusic from "../buttons/ScanForMusic";
+import MusicList from "./MusicList";
+import LocalBase from "localbase";
+import EventEmitter from "../../services/EventEmitter";
 
 function FavoriteScreen() {
+  const [musicList, setMusicList] = useState([]);
+  const db = new LocalBase("musicDB");
+
+  useEffect(() => {
+    loadTracksFromDB();
+
+    EventEmitter.on("tracksChanged", loadTracksFromDB);
+
+    return () => {
+      EventEmitter.off("tracksChanged", loadTracksFromDB);
+    };
+  }, []);
+
+  const loadTracksFromDB = async () => {
+    const favorites = (await db.collection("favoriteTracks").get()).map((fav) => fav.favoriteId);
+    const tracks = (await db.collection("tracks").get()).filter((track) => favorites.includes(track.id));
+    setMusicList(tracks);
+  };
 
   return (
     <>
+      {musicList.length !== 0 ? (
+        <div className="upload-music-list-container">
+          <MusicList musicList={musicList} />
+        </div>
+      ) : (
+        <div className="upload-music-buttons-container">
+          No Favorites Yet
+        </div>
+      )}
     </>
   );
 }
--- src/components/layout/MusicList.js
+++ src/components/layout/MusicList.js
@@ -1,16 +1,23 @@
-import "../../styles/layout/music_list.scss"
+import React from 'react';
+import { useLocation } from 'react-router-dom'; // Import useLocation to get current route
+import "../../styles/layout/music_list.scss";
 import ScanForMusic from "../buttons/ScanForMusic";
 import UploadMusic from "../buttons/UploadMusic";
 import MusicCard from "../cards/MusicCard";
 
 function MusicList({ musicList }) {
+    const location = useLocation();
+
+    const isFavoritesRoute = location.pathname === "/favorites";
 
     return (
         <div className="music-list-wrapper">
-            <div className="upload-music-icons-container">
-                <UploadMusic />
-                <ScanForMusic />
-            </div>
+            {!isFavoritesRoute && (
+                <div className="upload-music-icons-container">
+                    <UploadMusic />
+                    <ScanForMusic />
+                </div>
+            )}
             {musicList.length === 0 ? (<></>) : (
                 <>
                     {musicList.map((musicItem, index) => (

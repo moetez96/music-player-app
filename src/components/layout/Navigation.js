a42f3048ac532a86810ec988a64d76609de924f7
moetez96 <moetez.ayari@esprit.tn>
2024-05-12 01:04:28 +0100

    Fix navbar icons

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../../styles/layout/navigation.scss';
-import HomeIcon from '../icons/HomeIcon';
-import PlaylistIcon from '../icons/PlaylistIcon';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/layout/navigation.scss";
+import HomeIcon from "../icons/HomeIcon";
+import PlaylistIcon from "../icons/PlaylistIcon";
 
 function Navigation() {
-
-    return(
-        <div className="nav-wrapper">
-            <Link to="/">
-                <HomeIcon/>
-            </Link>
-            <Link to="/favorites">
-                <PlaylistIcon/>
-            </Link>
-        </div>
-    );
+  return (
+    <div className="nav-wrapper">
+      <Link to="/">
+        <HomeIcon />
+      </Link>
+      <Link to="/favorites">
+        <PlaylistIcon />
+      </Link>
+    </div>
+  );
 }
 
 export default Navigation;

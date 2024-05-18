1fbeb0d6211d484ed78088f0e4a63f8d2fd85061
moetez96 <moetez.ayari@esprit.tn>
2024-05-18 22:55:36 +0100

    Fix favoites page styling

--- src/components/layout/MusicList.js
+++ src/components/layout/MusicList.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { useLocation } from "react-router-dom";
 import "../../styles/layout/music_list.scss";
 import ScanForMusic from "../buttons/ScanForMusic";
 import UploadMusic from "../buttons/UploadMusic";
 import MusicCard from "../cards/MusicCard";
 
 function MusicList({ musicList }) {
-    const location = useLocation();
+  const location = useLocation();
 
-    const isFavoritesRoute = location.pathname === "/favorites";
+  const isFavoritesRoute = location.pathname === "/favorites";
 
-    return (
-        <div className="music-list-wrapper">
-            {!isFavoritesRoute && (
-                <div className="upload-music-icons-container">
-                    <UploadMusic />
-                    <ScanForMusic />
-                </div>
-            )}
-            {musicList.length === 0 ? (<></>) : (
-                <>
-                    {musicList.map((musicItem, index) => (
-                        <MusicCard key={index} musicItem={musicItem} />
-                    ))}
-                </>
-            )}
-        </div>
-    );
+  return (
+    <div className="music-list-wrapper">
+      <div className="upload-music-icons-container">
+        {!isFavoritesRoute && (
+          <>
+            <UploadMusic />
+            <ScanForMusic />
+          </>
+        )}
+      </div>
+
+      {musicList.length === 0 ? (
+        <></>
+      ) : (
+        <>
+          {musicList.map((musicItem, index) => (
+            <MusicCard key={index} musicItem={musicItem} />
+          ))}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default MusicList;

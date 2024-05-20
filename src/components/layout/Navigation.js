8c5d1fdbdf27e624b8226b369200333051420794
moetez96 <moetez.ayari@esprit.tn>
2024-05-20 16:15:53 +0100

    Fix navigation icons

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../../styles/layout/navigation.scss";
 import HomeIcon from "../icons/HomeIcon";
 import PlaylistIcon from "../icons/PlaylistIcon";
 
 function Navigation() {
+  const location = useLocation();
+  const isFavoritesRoute = location.pathname === "/favorites";
+
   return (
     <div className="nav-wrapper">
       <Link to="/">
-        <HomeIcon />
+        <HomeIcon isFavoritesRoute={isFavoritesRoute}/>
       </Link>
       <Link to="/favorites">
-        <PlaylistIcon />
+        <PlaylistIcon isFavoritesRoute={isFavoritesRoute}/>
       </Link>
     </div>
   );

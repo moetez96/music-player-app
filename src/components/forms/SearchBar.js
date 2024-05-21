68ac18b1d64f884e3d96fcbb79daab34c951c04a
moetez96 <moetez.ayari@esprit.tn>
2024-05-21 10:59:04 +0100

    Add close search icon

--- src/components/forms/SearchBar.js
+++ src/components/forms/SearchBar.js
@@ -1,17 +1,27 @@
-import '../../styles/components/forms.scss';
-import SearchIcon from '../../components/icons/SearchIcon';
+import "../../styles/components/forms.scss";
+import SearchIcon from "../../components/icons/SearchIcon";
+import CloseSearchIcon from "../icons/CloseSearchIcon";
 
-function SearchBar({searchSetTracks, searchText}) {
-
+function SearchBar({ searchSetTracks, searchText }) {
   const handleSearchChange = (event) => {
     event.preventDefault();
     searchSetTracks(event.target.value);
-  }
+  };
 
   return (
     <div className="search-bar-wrapper">
-      <SearchIcon/>
-      <input type="text" placeholder="Search" value={searchText} onChange={handleSearchChange}/>
+      <SearchIcon />
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchText}
+        onChange={handleSearchChange}
+      />
+      {searchText !== "" && (
+        <span className="close-search-bar" onClick={() => searchSetTracks("")}>
+          <CloseSearchIcon />
+        </span>
+      )}
     </div>
   );
 }
